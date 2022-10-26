ad80f7bf9048452c4db13ffa369ccb7c4789cdd6
datdang <38239103+traxanhkho@users.noreply.github.com>
2022-10-26 14:35:57 +0700

    Auto-generated commit [ci skip]

--- out-tsc/app/course/course.service.js
+++
@@ -1,6 +0,0 @@
-export class courseService {
-    getCourses() {
-        return ['course1', 'course2', 'course3'];
-    }
-}
-//# sourceMappingURL=course.service.js.map
--- out-tsc/app/input-format.directive.spec.js
+++
@@ -1,8 +0,0 @@
-describe('InputFormatDirective', () => {
-    it('should create an instance', () => {
-        // const directive = new InputFormatDirective();
-        // expect(directive).toBeTruthy();
-    });
-});
-export {};
-//# sourceMappingURL=input-format.directive.spec.js.map
